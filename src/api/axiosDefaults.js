d07160f4667085afdba626f61eb039d91302afdf
Anna Afijan <ann.afijan@gmail.com>
2024-08-23 16:26:40 +0000

    install axios

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drf-api-pp-0ae57f00f3cd.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
