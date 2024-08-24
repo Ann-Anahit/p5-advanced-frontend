8148f6070b3201a7332f6e40cadddf4a197ad3bd
Anna Afijan <ann.afijan@gmail.com>
2024-08-24 12:24:57 +0000

    change url in api file

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-pp.herokuapp.com/";
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || "https://drf-api-pp-0ae57f00f3cd.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
