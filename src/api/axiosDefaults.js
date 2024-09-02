d64feb4ce6076a067ce83d3ce255bd7ac12b0f51
Anna Afijan <ann.afijan@gmail.com>
2024-09-02 15:01:19 +0000

    access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || "https://drf-api-pp-0ae57f00f3cd.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
