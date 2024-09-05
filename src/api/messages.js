3e786fec46a82919aa87dafe62de0f5c925c2376
Anna Afijan <ann.afijan@gmail.com>
2024-09-05 22:55:01 +0000

    change message.js url

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,12 +1,10 @@
-// src/api/messages.js
-
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/messages/';
+const API_URL = 'https://8000-annanahit-drfapi-niz9191cenx.ws.codeinstitute-ide.net/messages/';
 
-// Function to get a single message
+axios.defaults.withCredentials = true; 
+
 export const getMessage = (id) => axios.get(`${API_URL}${id}/`);
-
 export const getMessages = (userId) => axios.get(`${API_URL}${userId}/`);
 export const sendMessage = (data) => axios.post(`${API_URL}create/`, data);
 export const markAsRead = (id) => axios.patch(`${API_URL}read/${id}/`);
