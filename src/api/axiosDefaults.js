89701e2267845ac761f54c885f2335d948603452
Anna Afijan <ann.afijan@gmail.com>
2024-09-05 08:25:31 +0000

    change axiosdef

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || "https://drf-api-pp-0ae57f00f3cd.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api-pp-0ae57f00f3cd.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
