2ed1a4fa9851e275a64de9a896c1d9985087bf16
Anna Afijan <ann.afijan@gmail.com>
2024-11-13 20:42:05 +0000

    default image change

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-pp-0ae57f00f3cd.herokuapp.com/";
+axios.defaults.baseURL = "https://8000-annanahit-drfapi-8hp2g0b8pys.ws.codeinstitute-ide.net/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
