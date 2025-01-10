4f8cc2f1a21ed4a894ebe00fe0254ff3f1e790af
Anna Afijan <ann.afijan@gmail.com>
2025-01-10 23:58:07 +0000

    add styles to category icons

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from "../styles/Categories.module.css";
 
 function Categories({ categories }) {
     return (
-        <div className="categories">
+        <div className={styles.categories}>
             {categories.map((category) => (
-                <div key={category.id} className="category">
+                <div key={category.id} className={styles.category}>
                     {category.icon ? (
-                        <FontAwesomeIcon icon={category.icon} className="category-icon" />
+                        <FontAwesomeIcon icon={category.icon} className={styles.categoryIcon} />
                     ) : (
-                        <FontAwesomeIcon icon="circle" className="category-icon" />
+                        <FontAwesomeIcon icon="circle" className={styles.categoryIcon} />
                     )}
                     <span>{category.name}</span>
                 </div>
