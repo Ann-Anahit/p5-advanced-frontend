d19e8390f6c07c62fd8fc9b66aa03ec576b9a799
Anna Afijan <ann.afijan@gmail.com>
2025-01-18 22:18:36 +0000

    redirect sign out to home after sign out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
@@ -10,22 +12,30 @@ import Avatar from "./Avatar";
 import axios from "axios";
 import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 import { removeTokenTimestamp } from "../utils/utils";
+import { useHistory } from "react-router-dom";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+    const history = useHistory();
 
     const { expanded, setExpanded, ref } = useClickOutsideToggle();
+    const [showModal, setShowModal] = useState(false);
 
     const handleSignOut = async () => {
         try {
             await axios.post("dj-rest-auth/logout/");
             setCurrentUser(null);
             removeTokenTimestamp();
+            setShowModal(false); // Close modal after successful sign-out
+            history.push("/");
         } catch (err) {
-            // console.log(err);  
+            console.error("Error during sign out:", err);
         }
     };
+
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
 
     const addPostIcon = (
         <NavLink
@@ -57,16 +67,6 @@ const NavBar = () => {
         </NavLink>
     );
 
-    const addLogoutIcon = (
-        <NavLink
-            className={styles.NavLink}
-            to="/"
-            onClick={handleSignOut}
-        >
-            <i className="fas fa-sign-out-alt"></i>Sign out
-        </NavLink>
-    );
-
     const loggedInIcons = (
         <>
             <NavLink
@@ -84,7 +84,14 @@ const NavBar = () => {
                 <i className="fas fa-heart"></i>Liked
             </NavLink>
             {EventIcon}
-            {addLogoutIcon}
+            <NavLink
+                className={styles.NavLink}
+                to="#"
+                onClick={handleShowModal}
+            >
+                <i className="fas fa-sign-out-alt"></i>Sign out
+            </NavLink>
+
             <NavLink
                 className={styles.NavLink}
                 to={`/profiles/${currentUser?.profile_id}`}
@@ -114,40 +121,58 @@ const NavBar = () => {
     );
 
     return (
-        <Navbar
-            expanded={expanded}
-            className={styles.NavBar}
-            expand="md"
-            fixed="top"
-        >
-            <Container>
-                <NavLink to="/">
-                    <Navbar.Brand>
-                        <img src={logo} alt="logo" height="45" />
-                    </Navbar.Brand>
-                </NavLink>
-                {currentUser && addPostIcon}
-                {currentUser && addEventIcon}
-                <Navbar.Toggle
-                    ref={ref}
-                    onClick={() => setExpanded(!expanded)}
-                    aria-controls="basic-navbar-nav"
-                />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto text-left">
-                        <NavLink
-                            exact
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                            to="/"
-                        >
-                            <i className="fas fa-home"></i>Home
-                        </NavLink>
-                        {currentUser ? loggedInIcons : loggedOutIcons}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            <Navbar
+                expanded={expanded}
+                className={styles.NavBar}
+                expand="md"
+                fixed="top"
+            >
+                <Container>
+                    <NavLink to="/">
+                        <Navbar.Brand>
+                            <img src={logo} alt="logo" height="45" />
+                        </Navbar.Brand>
+                    </NavLink>
+                    {currentUser && addPostIcon}
+                    {currentUser && addEventIcon}
+                    <Navbar.Toggle
+                        ref={ref}
+                        onClick={() => setExpanded(!expanded)}
+                        aria-controls="basic-navbar-nav"
+                    />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto text-left">
+                            <NavLink
+                                exact
+                                className={styles.NavLink}
+                                activeClassName={styles.Active}
+                                to="/"
+                            >
+                                <i className="fas fa-home"></i>Home
+                            </NavLink>
+                            {currentUser ? loggedInIcons : loggedOutIcons}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            {/* Sign-out confirmation modal */}
+            <Modal show={showModal} onHide={handleCloseModal} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Sign Out</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you want to sign out?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" onClick={handleSignOut}>
+                        Sign Out
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 };
 
