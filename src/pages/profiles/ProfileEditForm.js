35aa37a8dca4ba9101a687d878b4c7315c95954f
Anna Afijan <ann.afijan@gmail.com>
2025-01-19 13:40:04 +0000

    change the profile edit form

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useHistory, useParams } from "react-router-dom";
-
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
@@ -8,14 +7,10 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
-
 import { axiosReq } from "../../api/axiosDefaults";
-import {
-    useCurrentUser,
-    useSetCurrentUser,
-} from "../../contexts/CurrentUserContext";
-
+import { useCurrentUser, useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import btnStyles from "../../styles/Button.module.css";
+import styles from "../../styles/ProfileEdit.module.css";
 import appStyles from "../../App.module.css";
 
 const ProfileEditForm = () => {
@@ -25,13 +20,8 @@ const ProfileEditForm = () => {
     const history = useHistory();
     const imageFile = useRef();
 
-    const [profileData, setProfileData] = useState({
-        name: "",
-        content: "",
-        image: "",
-    });
+    const [profileData, setProfileData] = useState({ name: "", content: "", image: "" });
     const { name, content, image } = profileData;
-
     const [errors, setErrors] = useState({});
 
     useEffect(() => {
@@ -39,8 +29,7 @@ const ProfileEditForm = () => {
             if (currentUser?.profile_id?.toString() === id) {
                 try {
                     const { data } = await axiosReq.get(`/profiles/${id}/`);
-                    const { name, content, image } = data;
-                    setProfileData({ name, content, image });
+                    setProfileData(data);
                 } catch (err) {
                     console.log(err);
                     history.push("/");
@@ -49,15 +38,11 @@ const ProfileEditForm = () => {
                 history.push("/");
             }
         };
-
         handleMount();
     }, [currentUser, history, id]);
 
     const handleChange = (event) => {
-        setProfileData({
-            ...profileData,
-            [event.target.name]: event.target.value,
-        });
+        setProfileData({ ...profileData, [event.target.name]: event.target.value });
     };
 
     const handleSubmit = async (event) => {
@@ -67,15 +52,12 @@ const ProfileEditForm = () => {
         formData.append("content", content);
 
         if (imageFile?.current?.files[0]) {
-            formData.append("image", imageFile?.current?.files[0]);
+            formData.append("image", imageFile.current.files[0]);
         }
 
         try {
             const { data } = await axiosReq.put(`/profiles/${id}/`, formData);
-            setCurrentUser((currentUser) => ({
-                ...currentUser,
-                profile_image: data.image,
-            }));
+            setCurrentUser((currentUser) => ({ ...currentUser, profile_image: data.image }));
             history.goBack();
         } catch (err) {
             console.log(err);
@@ -83,82 +65,94 @@ const ProfileEditForm = () => {
         }
     };
 
-    const textFields = (
-        <>
-            <Form.Group>
-                <Form.Label>Bio</Form.Label>
-                <Form.Control
-                    as="textarea"
-                    value={content}
-                    onChange={handleChange}
-                    name="content"
-                    rows={7}
-                />
-            </Form.Group>
-
-            {errors?.content?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                    {message}
-                </Alert>
-            ))}
-            <Button
-                className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                onClick={() => history.goBack()}
-            >
-                cancel
-            </Button>
-            <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-                save
-            </Button>
-        </>
-    );
-
     return (
-        <Form onSubmit={handleSubmit}>
-            <Row>
-                <Col className="py-2 p-0 p-md-2 text-center" md={7} lg={6}>
-                    <Container className={appStyles.Content}>
+        <Row className={`justify-content-center ${appStyles.Row}`}>
+            <Col className="py-2 p-0 p-md-2 text-center" md={7} lg={6}>
+                <Form onSubmit={handleSubmit}>
+                    <Container className={`${appStyles.Content}`}>
+                        {/* Bio Section */}
                         <Form.Group>
-                            {image && (
-                                <figure>
-                                    <Image src={image} fluid />
-                                </figure>
-                            )}
-                            {errors?.image?.map((message, idx) => (
-                                <Alert variant="warning" key={idx}>
-                                    {message}
-                                </Alert>
-                            ))}
-                            <div>
+                            <Form.Label>Bio</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                value={content}
+                                onChange={handleChange}
+                                name="content"
+                                rows={7}
+                            />
+                        </Form.Group>
+                        {errors?.content?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+                        {/* Image Upload Section */}
+                        <Form.Group className="text-center">
+                            {image ? (
+                                <>
+                                    <figure>
+                                        <Image
+                                            className={styles.ProfileImage}
+                                            src={image}
+                                            rounded />
+                                    </figure>
+                                    <div>
+                                        <Form.Label
+                                            className={`${btnStyles.Button} ${btnStyles.Color}`}
+                                            htmlFor="image-upload"
+                                        >
+                                            Change the image
+                                        </Form.Label>
+                                    </div>
+                                </>
+                            ) : (
                                 <Form.Label
-                                    className={`${btnStyles.Button} ${btnStyles.Blue} btn my-auto`}
+                                    className="d-flex justify-content-center"
                                     htmlFor="image-upload"
                                 >
-                                    Change the image
+                                    Click or tap to upload an image
                                 </Form.Label>
-                            </div>
-                            <Form.File
+                            )}
+                            <Form.Control
                                 id="image-upload"
-                                ref={imageFile}
+                                className="d-none"
+                                type="file"
                                 accept="image/*"
                                 onChange={(e) => {
                                     if (e.target.files.length) {
                                         setProfileData({
                                             ...profileData,
-                                            image: URL.createObjectURL(e.target.files[0]),
+                                            image: URL.createObjectURL(e.target.files[0])
                                         });
                                     }
                                 }}
+                                ref={imageFile}
                             />
                         </Form.Group>
-                        <div className="d-md-none">{textFields}</div>
+                        {errors?.image?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+                        {/* Cancel and Save Buttons Section */}
+                        <div className="text-center">
+                            <Button
+                                className={`${btnStyles.Button} ${btnStyles.Blue}`}
+                                onClick={() => history.goBack()}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                className={`${btnStyles.Button} ${btnStyles.Blue}`}
+                                type="submit"
+                            >
+                                Save
+                            </Button>
+                        </div>
                     </Container>
-                </Col>
-                <Col md={5} lg={6} className="d-none d-md-block p-0 p-md-2 text-center">
-                    <Container className={appStyles.Content}>{textFields}</Container>
-                </Col>
-            </Row>
-        </Form>
+                </Form>
+            </Col>
+        </Row>
     );
 };
 
