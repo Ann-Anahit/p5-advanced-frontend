3cbf0e33809502a5cecbc450066a16e9bbb87cc8
Anna Afijan <ann.afijan@gmail.com>
2025-01-19 21:34:30 +0000

    redirect the logged out user to homepage

--- src/pages/event/EventRenderPage.js
+++ src/pages/event/EventRenderPage.js
@@ -1,23 +1,20 @@
 import React, { useEffect, useState } from "react";
-
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
 import Event from "./Events";
 import Asset from "../../components/Asset";
-
 import appStyles from "../../App.module.css";
 import styles from "../../styles/EventRenderPage.module.css";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Redirect } from "react-router-dom";
 
 function EventRenderPage({ message, filter = "" }) {
     const [events, setEvents] = useState({ results: [] });
@@ -25,10 +22,11 @@ function EventRenderPage({ message, filter = "" }) {
     const { pathname } = useLocation();
     const [query, setQuery] = useState("");
     const currentUser = useCurrentUser();
+
     useEffect(() => {
         const fetchEvents = async () => {
             try {
-                const { data } = await axiosReq.get(`/event/?${filter}search=${query}`);
+                const { data } = await axiosReq.get(`/event/?${filter}&search=${query}`);
                 setEvents(data);
                 setHasLoaded(true);
             } catch (err) {
@@ -45,6 +43,10 @@ function EventRenderPage({ message, filter = "" }) {
             clearTimeout(timer);
         };
     }, [filter, query, pathname, currentUser]);
+
+    if (!currentUser) {
+        return <Redirect to="/" />;
+    }
 
     return (
         <Row className="h-100">
