112dcd9a49dfc25c68532f58b7ed22d0f2808b83
Anna Afijan <ann.afijan@gmail.com>
2025-01-19 15:29:33 +0000

    Add sign out button and functionality in profile view.

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/MoreDropdown.module.css";
 import { useHistory } from "react-router";
+import axios from "axios";
+import { removeTokenTimestamp } from "../utils/utils";
+import { useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 // Dropdown needs access to the DOM node in order to position the Menu
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
@@ -44,32 +49,72 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
 };
 
 export const ProfileEditDropdown = ({ id }) => {
+  const setCurrentUser = useSetCurrentUser();
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
   const history = useHistory();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+      removeTokenTimestamp();
+      setShowModal(true);
+      history.push("/");
+    } catch (err) {
+      console.error("Error during sign out:", err);
+    }
+  };
+
   return (
-    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
-      <Dropdown.Toggle as={ThreeDots} />
-      <Dropdown.Menu>
-        <Dropdown.Item
-          onClick={() => history.push(`/profiles/${id}/edit`)}
-          aria-label="edit-profile"
-        >
-          <i className="fas fa-edit" /> edit profile
-        </Dropdown.Item>
-        <Dropdown.Item
-          onClick={() => history.push(`/profiles/${id}/edit/username`)}
-          aria-label="edit-username"
-        >
-          <i className="far fa-id-card" />
-          change username
-        </Dropdown.Item>
-        <Dropdown.Item
-          onClick={() => history.push(`/profiles/${id}/edit/password`)}
-          aria-label="edit-password"
-        >
-          <i className="fas fa-key" />
-          change password
-        </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <>
+      <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+        <Dropdown.Toggle as={ThreeDots} />
+        <Dropdown.Menu>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit`)}
+            aria-label="edit-profile"
+          >
+            <i className="fas fa-edit" /> edit profile
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/username`)}
+            aria-label="edit-username"
+          >
+            <i className="far fa-id-card" />
+            change username
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/password`)}
+            aria-label="edit-password"
+          >
+            <i className="fas fa-key" />
+            change password
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={handleShowModal}
+            aria-label="sign-out"
+          >
+            <i className="fa-solid fa-right-from-bracket" />
+            Sign out
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Sign Out</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to sign out?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleSignOut}>
+            Sign Out
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
